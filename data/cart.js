0459e40cb758fc51f119157992a54b53e1610073
khalidibnfaraj <khalidfarajdev@gmail.com>
2024-12-16 03:01:50 +0200

    Save date and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,11 +3,13 @@ if(!cart)
 {
 cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '1'
 },
 {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '2'
 }];
 }
 
@@ -36,7 +38,8 @@ export function AddItemToCart(productId)
     else{
         cart.push({
           productId,
-          quantity: Number(selectElement.value)
+          quantity: Number(selectElement.value),
+          deliveryOptionId: '1'
         });
       }
 
